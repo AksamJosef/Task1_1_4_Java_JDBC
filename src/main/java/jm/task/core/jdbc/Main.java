ad728ffc6b194965b276a92f115059ebbf673f82
AksamJosef <aksam.josef@gmail.com>
2023-03-21 12:07:23 +0600

    Hibernate first_try

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
 
@@ -16,22 +22,12 @@ public class Main {
         userService.createUsersTable();
 
 
+
         userService.saveUser("Nigmat", "Nigmatullin", (byte) 25);
-        System.out.println("User с именем Nigmat добавлен в базу данных");
-//        System.out.println("User с именем " + name + " добавлен в базу данных");
-
         userService.saveUser("Ayusha", "Nigmatullina", (byte) 25);
-        System.out.println("User с именем Ayusha добавлен в базу данных");
-
         userService.saveUser("Assaf Josef", "Nigmatullin", (byte) 1);
-        System.out.println("User с именем Assaf Josef добавлен в базу данных");
-
         userService.saveUser("Jotaro", "Kujo", (byte) 111);
-        System.out.println("User с именем Jotaro добавлен в базу данных");
 
-
-
-        System.out.println();
         userService.getAllUsers().stream()
                         .forEach(System.out::println);
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,16 +1,42 @@
 package jm.task.core.jdbc.util;
 
+//import java.lang.module.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Util {
     // реализуйте настройку соеденения с БД
 
-    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";//"com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/Users"; //NB  jdbc:mysql://HOST[:PORT][/DB]
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/Users";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "rootroot";
+
+
+    // Hibernate
+
+    public static SessionFactory getSession() {
+        return new Configuration()
+                .setProperty("hibernate.connection.url", DB_URL)
+                .setProperty("hibernate.connection.driver_class", DB_DRIVER)
+                .setProperty("hibernate.connection.username", DB_USERNAME)
+                .setProperty("hibernate.connection.password", DB_PASSWORD)
+//                .setProperty("hibernate.current_session_context_class", "tread")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                .setProperty("hibernate.show_sql", "true")
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+    }
+
+
+    // JDBC ----------
 
     public static Connection getConnection() {
         Connection connection = null;
